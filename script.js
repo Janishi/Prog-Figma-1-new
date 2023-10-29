04b6696a94439534a7bd09f4c2f1f637e865e12c
janishi <giorgijanishi@gmail.com>
2023-10-29 20:21:10 +0400

    finished section6

---
+++ script.js
@@ -0,0 +1,6 @@
+let sec5_button = document.getElementById('sec5_button');
+let sec5_block1_p3 = document.getElementById('sec5-block1-p3');
+
+sec5_button.addEventListener("click", function(){
+    sec5_block1_p3.classList.toggle('active');
+})
