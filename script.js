7140342ae3e58faab221d2035646cb8cafa96512
janishi <giorgijanishi@gmail.com>
2023-11-07 01:45:20 +0400

    started burger bar

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let sec5_button = document.getElementById('sec5_button');
 let sec5_block1_p3 = document.getElementById('sec5-block1-p3');
+let burger = document.getElementById('burger');
+let navigation = document.getElementById ("navigation");
 
 sec5_button.addEventListener("click", function(){
     sec5_block1_p3.classList.toggle('active');
+})
+
+burger.addEventListener("click", function(){
+    navigation.classList.toggle('active_nav');
 })
